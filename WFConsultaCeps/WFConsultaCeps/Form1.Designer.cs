e941ef2b77eccebc330aa0efe3e97e9163c605a7
Igor Gassen <104468900+Kryotsz@users.noreply.github.com>
2022-08-23 19:32:19 -0300

    Add files via upload

---
+++ WFConsultaCeps/WFConsultaCeps/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WFConsultaCeps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.btnBuscarCep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Endereço";
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEndereco.Location = new System.Drawing.Point(12, 33);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(262, 29);
+            this.txtEndereco.TabIndex = 1;
+            // 
+            // btnBuscarCep
+            // 
+            this.btnBuscarCep.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnBuscarCep.Location = new System.Drawing.Point(280, 33);
+            this.btnBuscarCep.Name = "btnBuscarCep";
+            this.btnBuscarCep.Size = new System.Drawing.Size(126, 29);
+            this.btnBuscarCep.TabIndex = 2;
+            this.btnBuscarCep.Text = "Buscar Cep";
+            this.btnBuscarCep.UseVisualStyleBackColor = true;
+            this.btnBuscarCep.Click += new System.EventHandler(this.btnBuscarCep_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 87);
+            this.Controls.Add(this.btnBuscarCep);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Button btnBuscarCep;
+    }
+}
+
---
+++ WFConsultaCeps/WFConsultaCeps/Form2.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace WFConsultaCeps
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCep = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBuscarCep = new System.Windows.Forms.Button();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtComplemento = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtComplemento2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCep
+            // 
+            this.txtCep.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCep.Location = new System.Drawing.Point(12, 33);
+            this.txtCep.Name = "txtCep";
+            this.txtCep.Size = new System.Drawing.Size(151, 29);
+            this.txtCep.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "CEP";
+            // 
+            // btnBuscarCep
+            // 
+            this.btnBuscarCep.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarCep.Location = new System.Drawing.Point(169, 33);
+            this.btnBuscarCep.Name = "btnBuscarCep";
+            this.btnBuscarCep.Size = new System.Drawing.Size(217, 29);
+            this.btnBuscarCep.TabIndex = 4;
+            this.btnBuscarCep.Text = "Buscar";
+            this.btnBuscarCep.UseVisualStyleBackColor = true;
+            this.btnBuscarCep.Click += new System.EventHandler(this.btnBuscarCep_Click);
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtEndereco.Location = new System.Drawing.Point(12, 89);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(374, 29);
+            this.txtEndereco.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Endereço";
+            // 
+            // txtComplemento
+            // 
+            this.txtComplemento.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtComplemento.Location = new System.Drawing.Point(12, 142);
+            this.txtComplemento.Name = "txtComplemento";
+            this.txtComplemento.Size = new System.Drawing.Size(374, 29);
+            this.txtComplemento.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(12, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Complemento";
+            // 
+            // txtComplemento2
+            // 
+            this.txtComplemento2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtComplemento2.Location = new System.Drawing.Point(12, 198);
+            this.txtComplemento2.Name = "txtComplemento2";
+            this.txtComplemento2.Size = new System.Drawing.Size(374, 29);
+            this.txtComplemento2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(12, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Complemento 2";
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtBairro.Location = new System.Drawing.Point(12, 254);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(374, 29);
+            this.txtBairro.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(12, 230);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Bairro";
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtCidade.Location = new System.Drawing.Point(12, 310);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(374, 29);
+            this.txtCidade.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(12, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Cidade";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtEstado.Location = new System.Drawing.Point(12, 366);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(374, 29);
+            this.txtEstado.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(12, 342);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 21);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Estado";
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.btnConfirmar.Location = new System.Drawing.Point(12, 401);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(374, 26);
+            this.btnConfirmar.TabIndex = 17;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 444);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtComplemento2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtComplemento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBuscarCep);
+            this.Controls.Add(this.txtCep);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Busca CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBuscarCep;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtComplemento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtComplemento2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnConfirmar;
+    }
+}
